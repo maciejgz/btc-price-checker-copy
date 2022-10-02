fa0dcf40e35c5967327698b804e8ca561c5bbd1b
maciejgz <macgzi@gmail.com>
2022-10-02 21:44:22 +0200

    initial data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert INTO btc.product (id, name, price, amount) values (1, 'wallet', 15.34, 500);
+insert INTO btc.product (id, name, price, amount) values (2, 'soap', 2, 2000);
+insert INTO btc.product (id, name, price, amount) values (3, 'wallet', 15.34, 5000);
+insert INTO btc.product (id, name, price, amount) values (4, 'soap', 2, 40);
